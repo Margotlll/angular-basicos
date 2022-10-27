596bf23a4ff849656ac1d63c3d2b251c988000a0
Margotlll <141155@unsaac.edu.pe>
2022-10-27 15:49:49 -0500

    gitlab

--- src/app/contador/contador/contador.component.ts
+++ src/app/contador/contador/contador.component.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
   selector: 'app-contador',
   template:
   `<h2>{{title}}</h2>
-  <h2>la base es: <span>{{base}}</span></h2>
-  <button (click)="acumular(base)">-{{base}}</button>
-  <h1>{{numero}}</h1>
-  <button (click)="acumular(-base)">+{{base}}</button>
+  <h2>la base es: <span>{{base}}</span></h2><button (click)="acumular(base)">-{{base}}</button>
+  <h1>{{numero}}</h1><button (click)="acumular(-base)">+{{base}}</button>
   `,
 })
 export class ContadorComponent{
